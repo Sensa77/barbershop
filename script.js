3acfa3ec8b8a87c8a2f51348a681e0945e751690
Sensa <77754618083@mail.ru>
2020-10-19 22:59:39 +0300

    сверстала и написала логику карты

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 var link = document.querySelector(".login-link");
 var popup = document.querySelector(".modal-login");
 var close = popup.querySelector(".modal-close");
-var form = popup .querySelector("form");
-var login = popup .querySelector("[name=login]");
-var password = popup .querySelector("[name=password]");
+var form = popup.querySelector("form");
+var login = popup.querySelector("#user-name");
+var password = popup.querySelector("#user-password");
 
-var isStorageSupport = true;
+var isStorageSupport = !0;
 var storage = "";
 
 try {
     storage = localStorage.getItem("login");
 } catch (err) {
-    isStorageSupport = false;
+    isStorageSupport = !1;
 }
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
-
     if (storage) {
         login.value = storage;
         password.focus();
@@ -44,14 +43,37 @@ form.addEventListener("submit", function (evt) {
             localStorage.setItem("login", login.value);
         }
     }
-})
+});
 
 window.addEventListener("keydown", function(evt) {
     if (evt.keyCode === 27) {
+        evt.preventDefault ();
         if (popup.classList.contains("modal-show")) {
-            evt.preventDefault ();
             popup.classList.remove("modal-show");
             popup.classList.remove("modal-error")
+        }
+    }
+});
+
+var mapLink = document.querySelector(".contacts-button-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        if (mapPopup.classList.contains("modal-show")) {
+            evt.preventDefault();
+            mapPopup.classList.remove("modal-show");
         }
     }
 })
